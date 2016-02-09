a042b943b7b04ddf0a6e2a7d2be5fad6643b6136
Mist02468 <mist@ptd.net>
2016-02-08 20:10:09 -0500

    Rearranging Code to Span Multiple Pages, For Maps Versus Tables

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -14,21 +14,33 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
-    protected $cityTable;
-
     public function indexAction()
     {
-        //get the Mapbox API key from my local file
-        $secrets     = parse_ini_file('config/autoload/localSecrets.ini');
-        $accessToken = $secrets['MapboxAccessToken'];
-        $projectId   = $secrets['MapboxProjectId'];
+        return new ViewModel();
+    }
 
+    public function mapsAction()
+    {
+      //get the Mapbox API key from my local file
+      $secrets     = parse_ini_file('config/autoload/localSecrets.ini');
+      $accessToken = $secrets['MapboxAccessToken'];
+      $projectId   = $secrets['MapboxProjectId'];
+
+      $serviceManager   = $this->getServiceLocator();
+      $cities           = $serviceManager->get('Application\Model\CityTable')->fetchAll();
+      $companyLocations = $serviceManager->get('Application\Model\CompanyLocationTable')->fetchAll();
+
+      return new ViewModel(array('mapboxAccessToken' => $accessToken, 'mapboxProjectId' => $projectId,
+                                 'cities' => $cities, 'companyLocations' => $companyLocations));
+    }
+
+    public function tablesAction()
+    {
         $serviceManager   = $this->getServiceLocator();
         $cities           = $serviceManager->get('Application\Model\CityTable')->fetchAll();
-        $cities->buffer();
         $companyLocations = $serviceManager->get('Application\Model\CompanyLocationTable')->fetchAll();
 
-        return new ViewModel(array('mapboxAccessToken' => $accessToken, 'mapboxProjectId' => $projectId,
-                                   'cities' => $cities, 'stateTable' => $serviceManager->get('Application\Model\StateTable'), 'companyLocations' => $companyLocations));
+        return new ViewModel(array('cities' => $cities, 'stateTable' => $serviceManager->get('Application\Model\StateTable'),
+                                   'companyLocations' => $companyLocations));
     }
 }
