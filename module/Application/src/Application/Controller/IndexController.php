7bde300a1d0aecd774d9dda4fc443e65512214ce
Mist02468 <mist@ptd.net>
2016-03-05 15:04:07 -0500

    Improved Page Load Experience By Using Ajax and Direct SQL Queries (rather than objects), Added More Comments, and Changed the Clustering Color Schemes

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -27,15 +27,7 @@ class IndexController extends AbstractActionController
       $accessToken = $secrets['MapboxAccessToken'];
       $projectId   = $secrets['MapboxProjectId'];
 
-      $serviceManager   = $this->getServiceLocator();
-      $cities           = $serviceManager->get('Application\Model\CityTable')->fetchAll();
-      $companyLocations = $serviceManager->get('Application\Model\CompanyLocationTable')->fetchAll();
-      $companyTable     = $serviceManager->get('Application\Model\CompanyTable');
-      $stateTable       = $serviceManager->get('Application\Model\StateTable');
-
-      return new ViewModel(array('mapboxAccessToken' => $accessToken, 'mapboxProjectId' => $projectId,
-                                 'cities' => $cities, 'companyLocations' => $companyLocations,
-                                 'companyTable' => $companyTable, 'stateTable' => $stateTable));
+      return new ViewModel(array('mapboxAccessToken' => $accessToken, 'mapboxProjectId' => $projectId));
     }
 
     public function tablesByCityAction()
@@ -48,8 +40,26 @@ class IndexController extends AbstractActionController
         return new ViewModel();
     }
 
+    public function mapsAjaxAction()
+    {
+        //select the city fields and use a join to get the city's state's name
+        $citySql = <<<SQL
+SELECT city.name AS cityName, state.name AS stateName, coordinatesNorth, coordinatesWest, walkScore, transitScore, avgTemp, population, landAreaSquareMiles
+FROM city
+INNER JOIN state ON state.id = city.state_id
+SQL;
+        //select the company location fields and use a join to get the company location's company's name
+        $companyLocationSql = <<<SQL
+SELECT company.name AS companyName, coordinatesNorth, coordinatesWest
+FROM companyLocation
+INNER JOIN company ON company.id = companyLocation.company_id
+SQL;
+        return $this->getAndPrepareData(array($citySql, $companyLocationSql));
+    }
+
     public function tablesByCityAjaxAction()
     {
+        //select the city fields, use a join to get the city's state's name, and use joins and a group by to get the list of companies for each city
         $sql = <<<SQL
 SELECT city.name AS cityName, state.name AS stateName, walkScore, transitScore, avgTemp, population, landAreaSquareMiles,
        GROUP_CONCAT(DISTINCT company.name ORDER BY company.name ASC SEPARATOR ';') AS companies
@@ -59,11 +69,12 @@ INNER JOIN companyLocation ON companyLocation.city_id = city.id
 INNER JOIN company ON company.id = companyLocation.company_id
 GROUP BY city.id
 SQL;
-        return $this->getAndPrepareData($sql);
+        return $this->getAndPrepareData(array($sql));
     }
 
     public function tablesByCompanyAjaxAction()
     {
+        //select the company field and use joins and a group by to get lists of cities and states
         $sql = <<<SQL
 SELECT company.name AS companyName,
        GROUP_CONCAT(DISTINCT city.name ORDER BY city.name ASC SEPARATOR ', ') AS cities,
@@ -74,19 +85,29 @@ INNER JOIN city ON city.id = companyLocation.city_id
 INNER JOIN state ON state.id = city.state_id
 GROUP BY company.id
 SQL;
-        return $this->getAndPrepareData($sql);
+        return $this->getAndPrepareData(array($sql));
     }
 
-    private function getAndPrepareData($sql)
+    private function getAndPrepareData(array $sqls)
     {
+        //get the db handle
         $db = $this->getServiceLocator()->get('db');
 
-        $stmt           = $db->query($sql);
-        $resultSet      = $stmt->execute();
-        $result         = $resultSet->getResource()->fetchAll(\PDO::FETCH_NAMED);
-        $resultPrepared = array("data" => $result);
+        //might have multiple results, if given multiple sql queries
+        $results = array();
+        foreach($sqls as $sql) {
+            $stmt      = $db->query($sql);
+            $resultSet = $stmt->execute();
+            $results[] = $resultSet->getResource()->fetchAll(\PDO::FETCH_NAMED); //get results as an associative array
+        }
 
-        $this->response->setContent(\Zend\Json\Json::encode($resultPrepared));
+        //if just one query result, create a data associative array for data tables
+        if (count($results) === 1) {
+            $results = array("data" => $results[0]);
+        }
+
+        //output JSON rather than HTML
+        $this->response->setContent(\Zend\Json\Json::encode($results));
         return $this->response;
     }
 }
